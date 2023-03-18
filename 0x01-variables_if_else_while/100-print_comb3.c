d8482f0e59d7351133242bad6c169c72e70db910
Timothy2566 <togunniyi5@gmail.com>
2023-03-18 07:23:48 -0700

    print combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0 except otherwise
+ */
+int main(void)
+{
+	int i;
+	int j;
 
-
+	for (i = 0 ; j < 10 ; j++)
+	{
+		for (j = 1 ; j < 10 ; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
