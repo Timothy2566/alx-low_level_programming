a0b55fc8fb900de40be791f99f7ed9592c96a6dd
Timothy2566 <togunniyi5@gmail.com>
2023-03-17 20:04:01 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main fuction for input and out put
+ *
+ * Return: alwways 0 except otherwise
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
