268b086963066c05fa3ce9fcf66956b6ce3207f3
Timothy2566 <togunniyi5@gmail.com>
2023-03-23 18:45:49 -0700

    number from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 up to 9
+ *
+ * Return: the number 0 - 9 when successful
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
+}
