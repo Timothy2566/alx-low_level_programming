b43ce67a63080534340a2f8044db5b74867e6801
Timothy2566 <togunniyi5@gmail.com>
2023-03-30 21:19:42 -0700

    smiles

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,6 +15,7 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
+void rev_string(char *n);
 
 
 #endif
