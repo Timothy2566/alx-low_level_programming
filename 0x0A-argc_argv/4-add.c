274c050119e37bdec870cf14ae08b87c24bb7041
Timothy2566 <togunniyi5@gmail.com>
2023-04-08 04:29:31 -0700

    i need rest

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the main function for the integer
+ * @argc: the first function parameter
+ * argument count
+ * @argv: the second function parameter
+ * argument vector
+ * Return: 1 when successful except otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int t;
+	int sum = 0;
+
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	else
+	{
+		for (t = 1; t < argc; t++)
+		{
+			if (!atoi(argv[t]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			else
+				sum = sum + atoi(argv[t]);
+		}
+		printf("%d\n", sum);
+	}
+
+	exit(EXIT_SUCCESS);
+}
