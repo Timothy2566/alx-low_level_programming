dfc281e485a536aa94b3d1e158b88a333b95f732
Timothy2566 <togunniyi5@gmail.com>
2023-04-11 06:29:22 -0700

    black

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -16,14 +16,15 @@ void simple_print_buffer(char *buffer, unsigned int size)
 	i = 0;
 	while (i < size)
 	{
-	if (i % 10)
-	{
-		printf(" ");
-	}
-	if (!(i % 10) && i)
-	{
-		printf("\n");
-	}
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+
 		printf("0x%02x", buffer[i]);
 		i++;
 	}
