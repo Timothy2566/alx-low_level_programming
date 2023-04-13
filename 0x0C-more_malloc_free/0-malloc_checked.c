c688d3dae982b966358c99bb6e7e30ee6c29a949
Timothy2566 <togunniyi5@gmail.com>
2023-04-13 04:11:32 -0700

    hope

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,7 @@
 
 /**
  * malloc_checked - function that allocates memory using malloc
- * @t: the size to be allocates
+ * @b: the size to be allocates
  *
  * Return: Returns a pointer to the allocated memory
  */
