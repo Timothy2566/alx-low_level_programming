d080af072e243e650a53005d684502e034468781
Timothy2566 <togunniyi5@gmail.com>
2023-04-17 05:17:38 -0700

    shirt

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that prints the name of the file
+ *
+ * Return: return 0 always upon succes
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}
