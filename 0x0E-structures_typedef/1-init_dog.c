10f9207403039ed5e3503c1a1564daab8a837860
Timothy2566 <togunniyi5@gmail.com>
2023-04-17 18:46:09 -0700

    peace like a river

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,13 +2,13 @@
 #include "dog.h"
 
 /**
- * werey_dog - function that initialize the variable
+ * init_dog - function that initialize the variable
  * @p: pointer
  * @name: the werey dogs name
  * @age:the werey age
  * @owner: absolute werey owner
  */
-void werey_dog(struct dog *p, char *name, float age, char *owner)
+void init_dog(struct dog *p, char *name, float age, char *owner)
 {
 	if (p == NULL)
 		p = malloc(sizeof(struct dog));
