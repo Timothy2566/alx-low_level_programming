a9446107a544508054d88679f643ae19b6519011
Timothy2566 <togunniyi5@gmail.com>
2023-04-17 18:29:39 -0700

    cheers

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - function that contain the dog basic info
+ * @name: the werey name
+ * @age: age to initialaize
+ * @owner: the werey dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
