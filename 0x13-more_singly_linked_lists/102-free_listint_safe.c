736f114562d604bb69d4c3c8586eafb26a42b6cd
Timothy2566 <togunniyi5@gmail.com>
2023-05-03 00:16:31 -0700

    rest is the best medicine

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - a function that frees a listint_t list.
+ * @m: function parameter
+ * Return: the size of the list that was free’d
+ */
+
+size_t free_listint_safe(listint_t **m)
+{
+	size_t size = 0;
+	int gap;
+
+	listint_t *josh;
+
+	if (!m || !*m)
+		return (0);
+
+	for (size = 0; *m; size++)
+	{
+		gap = *m - (*m)->next;
+
+		if (gap > 0)
+		{
+			josh = (*m)->next;
+			free(*m);
+			*m = josh;
+		}
+
+		else
+		{
+			free(*m);
+			*m = NULL;
+			break;
+		}
+	}
+
+	*m = NULL;
+
+	return (gap);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,7 +34,7 @@ size_t print_listint_safe(const listint_t *head);
 listint_t *reverse_listint(listint_t **head);
 size_t timothy_listint_size(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
-
+size_t free_listint_safe(listint_t **m);
 
 
 #endif
