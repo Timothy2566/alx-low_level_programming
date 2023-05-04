931be2cf0e554fd4bf2d2242bb5fff78760aa9d5
Timothy2566 <togunniyi5@gmail.com>
2023-05-04 11:12:39 -0700

    people

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@ unsigned int binary_to_uint(const char *m);
 int _putchar(char c);
 void print_binary(unsigned long int w);
 int get_bit(unsigned long int q, unsigned int index);
+int set_bit(unsigned long int *m, unsigned int index);
 
 
 #endif
