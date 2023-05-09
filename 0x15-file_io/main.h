71e73fc92abd3ffffd9b71f057f37fc4d87fa2b0
Timothy2566 <togunniyi5@gmail.com>
2023-05-08 21:14:20 -0700

    joy plenty

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,4 +13,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+char *make_cow(char *tin);
+void slip_tin(int tim);
+
+int main(int argc, char *argv[]);
+
 #endif
